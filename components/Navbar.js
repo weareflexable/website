2011cf1672050c9e0e3006cf33a9e5880c3c8f2a
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-17 11:43:59 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,12 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = () => {
   return (
     <nav class="bg-white px-2 sm:px-4 py-5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 ">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="http://localhost:3000//" class="flex items-center">
-          <img
-            src="/logo1.png"
-            class="mr-3 h-6 sm:h-9"
-            alt="Flexable Logo"
-          />
+          <img src="/logo1.png" class="mr-3 h-6 sm:h-9" alt="Flexable Logo" />
           <span class="self-center text-4xl italic font-semibold whitespace-nowrap dark:text-white">
             Flexable
           </span>
@@ -33,55 +30,50 @@ const Navbar = () => {
           id="navbar-sticky"
         >
           <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+              <Link to="home" smooth={true} class="cursor-pointer" duration={500}>
+                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    HOME
+                </h1>
+              </Link>
+            </li>
+         
             <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
-              >
-                HOME
-              </a>
+              <Link to="nightlife" smooth={true} class="cursor-pointer" duration={500}>
+                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  NIGHTLIFE
+                </h1>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                NIGHTLIFE
-              </a>
+              <Link to="restaurants" smooth={true} class="cursor-pointer" duration={500}>
+                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  RESTAURANTS
+                </h1>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                RESTAURANTS
-              </a>
+              <Link to="greeklife" smooth={true} class="cursor-pointer" duration={500}>
+                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  GREEKLIFE
+                </h1>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                GREEK LIFE
-              </a>
+              <Link to="about us" smooth={true} class="cursor-pointer" duration={500}>
+                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                 ABOUT US
+                </h1>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                ABOUT US
-              </a>
+              <Link to="partnerwithus" smooth={true} class="cursor-pointer" duration={500}>
+                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  PARTNER WITH US !
+                </h1>
+              </Link>
             </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                PARTNER WITH US
-              </a>
-            </li>
+            
           </ul>
         </div>
       </div>

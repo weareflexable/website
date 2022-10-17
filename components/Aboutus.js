d3f9db76c5946990f4b52777941912979ae370a9
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-17 11:41:26 +0530

    Update Aboutus.js

--- components/Aboutus.js
+++ components/Aboutus.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Aboutus = () => {
   return (
     <>
-    <div className='text-center text-black italic text-6xl mt-10 pt-10 font-bold'>
-      <h1>About us</h1>
-    </div>
-    <div className='pt-10 mt-10 text-[#8109f8] pb-10 text-center text-3xl mx-20 px-40 italic '>
-
-      Flexable partners with popular venues to get you in:<br></br> with line skips at your favorite bar,<br></br> with same-day bookings at your favorite restaurants,<br></br> and even party passes at Greek houses.
-
-
-      
-    </div>
-    <hr className="h-1 mx-20 animate-pulse bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  "/>
+      <div name='about us' className=" mt-10 pt-10">
+        <div className="text-center text-black  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  justify justify-center italic text-6xl mt-10 pt-10 font-bold">
+          <h1>About us</h1>
+        </div>
+        <div className="pt-10 mt-10 text-[#8109f8]  pb-10 text-center text-3xl mx-20 px-40 italic ">
+          Flexable partners with popular venues to get you in:<br></br> with
+          line skips at your favorite bar,<br></br> with same-day bookings at
+          your favorite restaurants,<br></br> and even party passes at Greek
+          houses.
+        </div>
+        <hr className="h-1 mx-20 animate-pulse bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  " />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Aboutus
+export default Aboutus;

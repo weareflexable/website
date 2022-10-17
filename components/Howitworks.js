c3423750658bd6af920b701aafdc52a57c1eb587
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-17 11:43:21 +0530

    Update Howitworks.js

--- components/Howitworks.js
+++ components/Howitworks.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Howitworks = () => {
   return (
-    <div className="bg-[#b66ffc] py-10 text-center">
+    <div className="bg-[#b66ffc] py-10 text-center ">
       <h1 className="pb-5 text-black bold text-4xl">How it works ?</h1>
       <div className="mx-24 py-8  text-2xl  bg-white rounded-full text-justify px-10">
         <ul className="list-disc italic mx-5">

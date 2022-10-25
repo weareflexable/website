e3f01f03083d11b20e9ac2e7c74115d1308b1630
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-25 15:01:42 +0530

    Update Howitworks.js

--- components/Howitworks.js
+++ components/Howitworks.js
@@ -4,7 +4,7 @@ const Howitworks = () => {
   return (
     <div className="bg-[#b66ffc] py-10 text-center ">
       <h1 className="pb-5 text-black bold text-4xl">How it works ?</h1>
-      <div className="mx-24 py-8  text-2xl  bg-white rounded-full text-justify px-10">
+      <div className="mx-24 py-8  text-2xl max-h-30 bg-white rounded-full text-justify px-10">
         <ul className="list-disc italic mx-5">
           <li>Pick your venue</li>
           <li>

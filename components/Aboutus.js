558c1cd9a280e0a9b9434dee2cf3693f4ebd406d
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-25 15:10:24 +0530

    Update Aboutus.js

--- components/Aboutus.js
+++ components/Aboutus.js
@@ -6,7 +6,7 @@ const Aboutus = () => {
       <div name="about us" className=" mt-10 pt-10">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-6 lg:px-6">
           <div class="max-w-screen-1200pxl text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-6xl tracking-tight font-bold text-center text-gray-900 dark:text-white">
+            <h2 class="mb-4 italic text-6xl tracking-tight font-bold text-center text-gray-900 dark:text-white">
               About us
             </h2>
             <p class="mb-4 text-3xl italic ml-5 mt-5 pt-5 text-[#8109f8]">

abca4befa3abb4232ba07f6277a97c83066fc084
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-25 15:02:10 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,83 +1,145 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  const handleClose = () => setNav(!nav);
+
   return (
-    <nav class="bg-white px-2 sm:px-4 py-5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 ">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="http://localhost:3000//" class="flex items-center">
-          <img src="/logo1.png" class="mr-3 h-6 sm:h-9" alt="Flexable Logo" />
-          <span class="self-center text-4xl italic font-semibold whitespace-nowrap dark:text-white">
-            Flexable
-          </span>
-        </a>
-        <div class="flex md:order-2  items-center">
-          <a
-            href="#"
-            class="text-gray-800 text-2xl  font-semibold hover:text-purple-600 mr-4"
-          >
-            Login
-          </a>
-          <a
-            href="#"
-            class="text-gray-800 text-2xl  font-semibold border px-4 py-1 rounded-full hover:text-purple-600 hover:border-purple-600"
-          >
-            Register
-          </a>
-        </div>
-        <div
-          class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li>
-              <Link to="home" smooth={true} class="cursor-pointer" duration={500}>
-                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                    HOME
-                </h1>
-              </Link>
-            </li>
-         
+    <div className="w-screen h-[80px] z-10 bg-gradient-to-r from-indigo-200 via-indigo-400 to-indigo-200 fixed drop-shadow-lg">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center">
+          <h1 className="text-3xl font-bold italic mr-4 sm:text-4xl">Flexable</h1>
+          <ul className="hidden md:flex font-serif mx-10 text-xl p-5 md:space-x-8">
             <li>
-              <Link to="nightlife" smooth={true} class="cursor-pointer" duration={500}>
-                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  NIGHTLIFE
-                </h1>
+              <Link to="home" smooth={true} duration={500} class="cursor-pointer">
+                Home
               </Link>
             </li>
             <li>
-              <Link to="restaurants" smooth={true} class="cursor-pointer" duration={500}>
-                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  RESTAURANTS
-                </h1>
+              <Link to="nightlife" smooth={true} offset={-200} duration={500} class="cursor-pointer">
+                Nightlife
               </Link>
             </li>
             <li>
-              <Link to="greeklife" smooth={true} class="cursor-pointer" duration={500}>
-                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  GREEKLIFE
-                </h1>
+              <Link to="restaurants" smooth={true} offset={-50} duration={500} class="cursor-pointer">
+                Restaurants
               </Link>
             </li>
             <li>
-              <Link to="about us" smooth={true} class="cursor-pointer" duration={500}>
-                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                 ABOUT US
-                </h1>
+              <Link to="greeklife" smooth={true} offset={-100} duration={500} class="cursor-pointer">
+                Greeklife
               </Link>
             </li>
             <li>
-              <Link to="partnerwithus" smooth={true} class="cursor-pointer" duration={500}>
-                <h1 className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  PARTNER WITH US !
-                </h1>
+              <Link to="about us" smooth={true} offset={-50} duration={500} class="cursor-pointer">
+                About us
               </Link>
             </li>
-            
+            <li>
+              <Link
+                to="partnerwithus"
+                smooth={true}
+                offset={-50}
+                duration={500}
+                class="cursor-pointer"
+              >
+                Partner with us !
+              </Link>
+            </li>
           </ul>
         </div>
+        <div className="hidden md:flex pr-4">
+          <button className="border px-4 py-1 rounded-full hover:text-purple-600 hover:border-purple-600 italic bg-transparent text-xl text-black mr-4">
+            Sign In
+          </button>
+          <button className="px-8 italic border rounded-full hover:text-purple-600 hover:border-purple-600 text-xl py-3">Sign Up</button>
+        </div>
+        <div className="md:hidden mr-4" onClick={handleClick}>
+          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
+        </div>
       </div>
-    </nav>
+
+      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link onClick={handleClose} to="home" smooth={true} duration={500} class="cursor-pointer"
+          >
+            Home
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="nightlife"
+            smooth={true}
+            offset={-200}
+            duration={500}
+            class="cursor-pointer"
+          >
+            Nightlife
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="restaurants"
+            smooth={true}
+            offset={-50}
+            duration={500}
+            class="cursor-pointer"
+          >
+            Restaurants
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="greeklife"
+            smooth={true}
+            offset={-100}
+            duration={500}
+            class="cursor-pointer"
+          >
+            Greeklife
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="about us"
+            smooth={true}
+            offset={-50}
+            duration={500}
+            class="cursor-pointer"
+          >
+           About us
+          </Link>
+        </li>
+        <li className="border-b-2 border-zinc-300 w-full">
+          <Link
+            onClick={handleClose}
+            to="partnerwithus"
+            smooth={true}
+            offset={-50}
+            duration={500}
+            class="cursor-pointer"
+          >
+          Partner with us !
+          </Link>
+        </li>
+
+        <div className="flex flex-col my-4">
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+      </ul>
+    </div>
   );
 };
 

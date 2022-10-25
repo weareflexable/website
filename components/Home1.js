f2f1f8075b5f0a1f0b7f3dfe016c6d4787f06c66
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-25 15:00:57 +0530

    Update Home1.js

--- components/Home1.js
+++ components/Home1.js
@@ -1,16 +1,17 @@
 import React from "react";
 
+
 const Home1 = () => {
   return (
     <div name ="home">
-      <div className="w-full bg-cover  bg-white py-16 px-4">
+      <div className="w-full bg-cover  bg-white py-20 pt-15 mt-15 px-4">
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
           <div className="flex flex-col justify-center">
             <p className="text-[#8109f8] text-2xl font-bold ">
               Explore the world
             </p>
             <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
-              It's Time to enjoy with Flexable !
+              It&apos;s Time to enjoy with Flexable !
             </h1>
             <p>Discover the best places near your locality</p>
             <button className="bg-[#8109f8] text-white w-[200px] rounded-full font-medium my-6 mx-auto md:mx-0 py-3">

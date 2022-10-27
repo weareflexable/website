27afa09037ff87f8665e7014b13b8715cefe113c
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-27 09:00:32 +0530

    Update Aboutus.js

--- components/Aboutus.js
+++ components/Aboutus.js
@@ -3,13 +3,13 @@ import React from "react";
 const Aboutus = () => {
   return (
     <>
-      <div name="about us" className=" mt-10 pt-10">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-6 lg:px-6">
+      <div name="about us" className=" mt-5 pt-5">
+        <div class="pt-3 px-4 mx-auto max-w-screen-xl lg:py-6 lg:px-6">
           <div class="max-w-screen-1200pxl text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 italic text-6xl tracking-tight font-bold text-center text-gray-900 dark:text-white">
+            <h2 class="mb-4 italic text-6xl tracking-tight font-bold text-center text-black dark:text-white">
               About us
             </h2>
-            <p class="mb-4 text-3xl italic ml-5 mt-5 pt-5 text-[#8109f8]">
+            <p class="mb-4 text-3xl italic ml-5 text-justify  pt-8 text-[#8109f8]">
               Flexable partners with popular venues to get you in: with line
               skips at your favorite bar, with same-day bookings at your
               favorite restaurants, and even party passes at Greek houses.
@@ -26,4 +26,3 @@ const Aboutus = () => {
 };
 
 export default Aboutus;
-

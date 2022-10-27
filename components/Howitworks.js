f4a5dcecbf639720c39275f4926bd0fbc6fe9a5a
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-27 09:15:01 +0530

    Update Howitworks.js

--- components/Howitworks.js
+++ components/Howitworks.js
@@ -11,21 +11,21 @@ const Howitworks = () => {
             </h2>
           </div>
 
-          <div className="grid md:grid-cols-4 gap-4   mt-8 pt-8 pb-20 text-center">
-            <div className="border py-8 rounded-xl shadow-xl hover:scale-105 duration-300">
+          <div className="grid md:grid-cols-4 gap-4 pl-2 mt-8 pt-8 pb-20 text-center">
+            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
               <p className="text-black text-2xl  mt-2">Step 1</p>
               <p className="text-3xl mt-3 italic  text-[#8109f8]">Pick your venue</p>
             </div>
-            <div className="border py-8 rounded-xl shadow-xl hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl hover:scale-105 duration-300">
               <p className="text-black  text-2xl  mt-2">Step 2</p>
               <p className="text-3xl mt-3 italic  text-[#8109f8]">Purchase your Digital Access Token (DAT)-<br></br>Pay with credit card, Apple Pay & Google Pay
 </p>
             </div>
-            <div className="border py-8 rounded-xl shadow-xl hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
               <p className="text-black  text-2xl  mt-2">Step 3</p>
               <p className="text-3xl mt-3 italic  text-[#8109f8]">Locate your DAT in your wallet</p>
             </div>
-            <div className="border py-8 rounded-xl shadow-xl hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
               <p className="text-black  text-2xl  mt-2">Step 4</p>
               <p className="text-3xl mt-3 italic  text-[#8109f8]">Redeem your DAT through a QR code</p>
             </div>

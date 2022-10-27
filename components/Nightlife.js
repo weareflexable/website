3f6bf19f1d8de88ac8dd1648849c66fd751e2627
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-27 09:05:58 +0530

    Update Nightlife.js

--- components/Nightlife.js
+++ components/Nightlife.js
@@ -3,20 +3,18 @@ import React from "react";
 const Nightlife = () => {
   return (
     <>
-      <div name='nightlife' className="bg-white  mt-12 pt-12">
-        <div className="items-center text-center pt-10">
-          <h1 className="text-black italic text-6xl font-bold">
-            Nightlife
-          </h1>
+      <div name="nightlife" className="bg-white  mt-5 pt-5">
+        <div className="items-center text-center pt-3">
+          <h1 className="text-black italic text-6xl font-bold">Nightlife</h1>
           <img
-            className="w-[1000px] mx-auto my-4 mt-20 pt-10"
+            className="w-[1000px] mx-auto my-4 mt-8 pt-8"
             src="/nightlife2.jpg"
             alt="/"
           />
         </div>
-        <div className="max-w-[1240px] text-[#8109f8] pb-10 text-justify mx-auto pt-8 grid md:grid-cols-2">
+        {/* <div className="max-w-[1240px] text-[#8109f8] pb-10 text-justify mx-auto pt-8 grid md:grid-cols-2">
           <p className="text-3xl mx-10 italic">
-            Who doesn't want exclusive access? Party like it’s 1999 with line
+            Who doesn&apos;t want exclusive access? Party like it&apos;s 1999 with line
             skips, drink specials, event tickets, and more.
           </p>
           <p className="text-3xl mx-10 italic">
@@ -29,9 +27,21 @@ const Nightlife = () => {
             Search
           </button>
           </p>
+        </div> */}
+        <div className="max-w-[1240px] text-[#8109f8] pb-10 text-justify  mx-auto pt-8 ">
+          
+          <p className="text-3xl mx-10 italic">
+            Who doesn&apos;t want exclusive access? Party like it&apos;s 1999
+            with line skips, drink specials, event tickets, and more. Click
+            below to find participating venues in your area.
+            <br />
+           
+          </p>
+          <div className="flex justify-center mt-5"> <button className=' text-2xl border rounded-full  bg-purple-600 hover:bg-purple-800 shadow-lg p-2 text-white w-40  mt-2'>Search</button></div>
+         
         </div>
       </div>
-      <hr className="h-1 mx-20 animate-pulse bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  "/>
+      <hr className="h-1 mx-20 animate-pulse bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  " />
     </>
   );
 };

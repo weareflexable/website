475b6db8ad85fca67f39889ea6ef8d6cfef1aedc
ss251 <ss251@uw.edu>
2022-12-11 04:25:55 +0530

    add animations to opening

--- components/Hero.js
+++ components/Hero.js
@@ -2,27 +2,35 @@ import React from "react";
 import Container from "./Container";
 import Button from "./Button";
 import Image from "next/image";
+import { JackInTheBox, Reveal } from "react-awesome-reveal";
+import { fadeInDownShorter, fadeInLeft, fadeInUp } from "../keyframes";
 
 const Hero = () => {
   return (
-    <Container className={"flex flex-col sm:flex-row mt-20 mb-32"}>
-      <div className="w-full sm:w-[50%] pr-0 sm:pr-10 lg:pr-20 mt-[-100px] lg:mt-10">
-        <h1 className="text-3xl lg:text-7xl font-figtree font-bold mb-5">
-          Explore the world - it&apos;s time to enjoy with Flexable !
-        </h1>
-        <p className="mb-8 opacity-50 font-figtree">
-          Discover the best places near your locality
-        </p>
+    <Container id="collections" className={"flex flex-col sm:flex-row mb-32"}>
+      <div className="w-full sm:w-[50%] pr-0 sm:pr-10 lg:pr-20 mt-[-50px] lg:mt-10">
+        <Reveal keyframes={fadeInLeft} duration={800} delay={200}>
+          <h1 className="text-3xl lg:text-7xl font-figtree font-bold mb-5">
+            Explore the world - it&apos;s time to enjoy with Flexable !
+          </h1>
+        </Reveal>
+        <Reveal keyframes={fadeInDownShorter} duration={1000} delay={800}>
+          <p className="mb-8 opacity-50 font-figtree">
+            Discover the best places near your locality
+          </p>
+        </Reveal>
 
         <div className="flex mb-0">
-          <Button
-            isLink
-            href={"https://marketplace.flexabledats.com/"}
-            className={"mr-3 text-black"}
-            variant={"primary"}
-          >
-            Explore Now
-          </Button>
+          <Reveal keyframes={fadeInUp} duration={1000} delay={200}>
+            <Button
+              isLink
+              href={"https://marketplace.flexabledats.com/"}
+              className={"mr-3 text-black"}
+              variant={"primary"}
+            >
+              Explore Now
+            </Button>
+          </Reveal>
         </div>
       </div>
       <div className="flex-1 mt-4 lg:mt-0">

4105c6cb7d7d0da0fbc772ac2c64f0c5dba117ca
ss251 <ss251@uw.edu>
2022-12-11 17:47:43 +0530

    add about section

--- components/About.js
+++ components/About.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Container from "./Container";
+import { Reveal } from "react-awesome-reveal";
+import { fadeInDownShorter, fadeInLeft, fadeInUp } from "../keyframes";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container id="about" className={"mb-48 mt-20 lg:mt-24 scroll-mt-10"}>
+      <div className="flex flex-col items-center ">
+        <div className="text-center mb-12">
+          <h2 className="font-bold text-4xl lg:text-6xl mb-2">About</h2>
+        </div>
+        <Reveal keyframes={fadeInDownShorter} duration={800} delay={200}>
+          <div className="w-[50%] lg:ml-80">
+            <h1 className="text-2xl lg:text-4xl font-figtree font-bold opacity-50">
+              Flexable partners with popular venues to get you in: with line
+              skips at your favorite bar, with same-day bookings at your
+              favorite restaurants, and even party passes at Greek houses.
+            </h1>
+          </div>
+        </Reveal>
+      </div>
+    </Container>
+  );
 };
 
 export default About;

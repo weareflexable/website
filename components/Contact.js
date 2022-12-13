4f54388adb4e2760d9f75f878b6c4e0a8abec558
ss251 <ss251@uw.edu>
2022-12-13 06:27:01 +0530

    retain contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,135 @@
 import React from "react";
+import Button from "./Button";
+import Container from "./Container";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const Partner = () => {
+  return (
+    <Container id="contact" className={"mb-48 mt-20 lg:mt-24 scroll-mt-10"}>
+      <div className="text-center mb-12">
+        <h2 className="font-bold text-4xl lg:text-6xl mb-2">Contact Us</h2>
+      </div>
+      <div className="max-w-[1240px] m-auto pt-2 pb-5">
+        <form className="max-w-[500px] m-auto pl-2">
+          <div class="relative z-0 mb-6 w-full group">
+            <input
+              type="email"
+              name="floating_email"
+              id="floating_email"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required=""
+            />
+            <label
+              for="floating_email"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email address
+            </label>
+          </div>
+
+          <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="text"
+                name="floating_first_name"
+                id="floating_first_name"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+              />
+              <label
+                for="floating_first_name"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                First name
+              </label>
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="text"
+                name="floating_last_name"
+                id="floating_last_name"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+              />
+              <label
+                for="floating_last_name"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Last name
+              </label>
+            </div>
+          </div>
+          <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                name="floating_phone"
+                id="floating_phone"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+              />
+              <label
+                for="floating_phone"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Phone number
+              </label>
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="text"
+                name="floating_company"
+                id="floating_company"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+              />
+              <label
+                for="floating_company"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Business Name
+              </label>
+            </div>
+          </div>
+          <textarea
+            className="border shadow-lg p-3 w-full"
+            cols="30"
+            rows="8"
+            placeholder="Message"
+          ></textarea>
+          <div className="flex justify-center mt-5">
+            <Button
+              isLink
+              href={"#"}
+              className={"mr-3 text-black"}
+              variant={"primary"}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+      {/* <div className='max-w-[1240px] m-auto pt-2 pb-5'>
+        
+        <form className='max-w-[600px] m-auto '>
+            <div className='grid grid-cols-2 gap-2'>
+                <input className='border shadow-lg p-3' type="text" placeholder='Name' />
+                
+                <input className='border shadow-lg p-3' type="email" placeholder='Email' />
+               
+            </div>
+            <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Business Name' />
+            <textarea className='border shadow-lg p-3 w-full' cols="30" rows="8" placeholder='Message'></textarea>
+            <button className=' text-2xl border bg-purple-600 hover:bg-purple-800 shadow-lg p-2 text-white w-full mt-2'>Submit</button>
+        </form>
+    </div> */}
+    </Container>
+  );
 };
 
-export default Contact;
+export default Partner;
--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export default function Home() {
       <Events />
       <About />
       <Partner />
+      <Contact />
       <Footer />
     </div>
   );

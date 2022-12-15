ee67a1947dffa26f4d721eda70ea6c9517aa63f2
ss251 <ss251@uw.edu>
2022-12-15 05:47:37 +0530

    remove preview text

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -356,7 +356,7 @@ async function sendEmail(req, res) {
     </style>
   </head>
   <body>
-    <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
+    <span class="preheader"></span>
     <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
       <tr>
         <td>&nbsp;</td>

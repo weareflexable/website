98b846307fe55811fc5d9422712c0c288471df92
ss251 <ss251@uw.edu>
2022-12-17 05:35:26 +0530

    change partner section image and text

--- components/Partner.js
+++ components/Partner.js
@@ -5,7 +5,7 @@ import { fadeInDownShorter, fadeInLeft, fadeInUp } from "../keyframes";
 
 const Partner = () => {
   return (
-    <div className="bg-partner bg-left-center bg-left-center pb-24 pt-24">
+    <div className="bg-partner_mobile lg:bg-partner bg-center-top lg:bg-left pt-24 lg:pt-28">
       <Container id="partner" className={"mb-48 scroll-mt-10"}>
         <div className="flex flex-col items-center ">
           <div className="text-center mb-12">
@@ -14,33 +14,39 @@ const Partner = () => {
             </h2>
           </div>
           <Reveal keyframes={fadeInDownShorter} duration={500} delay={200}>
-            <div className="w-[50%] lg:ml-80 mb-8">
-              <h1 className="text-2xl lg:text-4xl font-figtree font-bold opacity-60">
-                The problem: Venue owners are limited to the amount of space
-                they own and operate. Until now, revenue was limited solely to
-                the services they provide their patrons.
+            <div className="lg:w-[50%] lg:ml-80 mb-8">
+              <h2 className="font-bold text-3xl lg:text-5xl mb-4">
+                The Problem
+              </h2>
+              <h1 className="text-xl lg:text-3xl font-figtree font-bold opacity-80 lg:opacity-70">
+                Venue owners are limited to the amount of space they own and
+                operate. Until now, revenue was limited solely to the services
+                they provide their patrons.
               </h1>
             </div>
-            <div className="w-[50%] lg:ml-80 mb-8">
-              <h1 className="text-2xl lg:text-4xl font-figtree font-bold opacity-60">
-                The solution- Through the sale of custom-designed NFTs, Flexable
-                has pioneered a system for venue owners to provide customers
-                with exclusive access to their services- capitalizing on the
-                venue's existing popularity and increasing revenue all without
-                changing the business model.
+            <div className="lg:w-[50%] lg:ml-80 mb-8">
+              <h2 className="font-bold text-3xl lg:text-5xl mb-4">
+                The Solution
+              </h2>
+              <h1 className="text-xl lg:text-3xl font-figtree font-bold opacity-80 lg:opacity-70">
+                Through the sale of custom-designed NFTs, Flexable has pioneered
+                a system for venue owners to provide customers with exclusive
+                access to their services- capitalizing on the venue's existing
+                popularity and increasing revenue all without changing the
+                business model.
               </h1>
             </div>
-            <div className="w-[50%] lg:ml-80">
-              <h1 className="text-2xl lg:text-4xl font-figtree font-bold opacity-60 mb-8">
+            <div className="lg:w-[50%] w-[75%] lg:ml-80">
+              <h1 className="text-xl lg:text-3xl font-figtree font-bold opacity-100 lg:opacity-70 lg:mb-8 mb-4">
                 Are customers clamoring to get into your establishment? We can
                 help you get them in.
               </h1>
-              <h1 className="text-2xl lg:text-4xl font-figtree font-bold opacity-60 mb-8">
+              <h1 className="text-xl lg:text-3xl font-figtree font-bold opacity-80 lg:opacity-70 mb-4">
                 “Flexable has created passive income for my venue where I didn't
                 know it existed before” - You, the venue owner, shortly after
                 partnering with us
               </h1>
-              <h1 className="text-2xl lg:text-4xl font-figtree font-bold opacity-60 mb-8">
+              <h1 className="text-xl lg:text-3xl font-figtree font-bold opacity-80 lg:opacity-70 mb-8">
                 “I literally only go to bars that are partnered with Flexable
                 now. I love the utility!” - Someone who previously waited in
                 lines

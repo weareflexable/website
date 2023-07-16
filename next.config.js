29e32e7b6e9e082417ab1ea1d90e528c4307e3fe
mbappai <mujahid.bappai@yahoo.com>
2023-07-16 17:09:49 +0100

    refactor: add new next.js config

--- next.config.js
+++ next.config.js
@@ -7,22 +7,35 @@
 
 // module.exports = nextConfig
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+// let assetPrefix = ''
+// let basePath = '/'
 
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
+
+
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/launchpad',
+
+  staticPageGenerationTimeout:  100,
+ output:"standalone",
+ images:{
+  loader:'akamai',
+  path:"",
+ },
 }
-
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
+module.exports = nextConfig

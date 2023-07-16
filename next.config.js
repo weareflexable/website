a65d8de0298a5fa7b7a92bb94b0c75f2d8d5b69f
mbappai <mujahid.bappai@yahoo.com>
2023-07-16 16:47:17 +0100

    refactor: move assests to image folder and add new gh workflow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "standalone",
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   output: "standalone",
+// }
+
+// module.exports = nextConfig
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}

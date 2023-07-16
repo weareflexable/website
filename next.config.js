4c28fd83beb2ac53341172bdc139627312a1237e
mbappai <mujahid.bappai@yahoo.com>
2023-07-16 18:10:04 +0100

    refactor: change github workflow

--- next.config.js
+++ next.config.js
@@ -7,35 +7,24 @@
 
 // module.exports = nextConfig
 
-// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-// let assetPrefix = ''
-// let basePath = '/'
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
+let assetPrefix = ''
+let basePath = '/'
 
-// if (isGithubActions) {
-//   // trim off `<owner>/`
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-//   assetPrefix = `/${repo}/`
-//   basePath = `/${repo}`
-// }
-
-
-// module.exports = {
-//   assetPrefix: assetPrefix,
-//   basePath: basePath,
-// }
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: "",
-
-  staticPageGenerationTimeout:  100,
- output:"standalone",
- images:{
-  loader:'akamai',
-  path:"/images",
- },
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
-module.exports = nextConfig
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'mujeex-1006.imgix.net',
+  },
+}

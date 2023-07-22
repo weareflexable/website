d4e9bf1a284bdf1031b844fe996ee3b6a3af8c8c
mbappai <mujahid.bappai@yahoo.com>
2023-07-22 07:41:17 +0100

    refactor: revert next config file to default

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   output: "standalone",
-// }
-
-// module.exports = nextConfig
-
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone",
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-}
+module.exports = nextConfig
+

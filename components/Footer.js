2cb8f56bc915ab988d6e92c6de028dc21dc15fa7
mbappai <mujahid.bappai@yahoo.com>
2023-08-04 15:03:15 +0100

    coppy: update footer text

--- components/Footer.js
+++ components/Footer.js
@@ -92,7 +92,7 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2022{" "}
+            © 2023{" "}
             <Link href="/" className="hover:underline">
               <a>Flexable</a>
             </Link>
